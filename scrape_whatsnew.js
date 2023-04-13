6de17757206ebd890e62a26dc2222de0ab0f10bc
@ugurkocde <ugur.koc@protonmail.com>
2023-04-13 15:41:51 +0200

    fix pupo

--- scrape_whatsnew.js
+++ scrape_whatsnew.js
@@ -4,7 +4,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function fetchWhatsNew() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
 
   await page.goto(
