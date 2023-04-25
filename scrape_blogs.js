93f732a47692dd65369e783171ebe7a4ac834b09
@ugurkocde <ugur.koc@protonmail.com>
2023-04-25 22:04:35 +0200

    fix text

--- scrape_blogs.js
+++ scrape_blogs.js
@@ -73,7 +73,7 @@ async function fetchBlogPosts() {
         await browser.close();
       }
 
-      const prompt = `Summarize the following text. Dont use the title to summarize the article. Focus on the content. Your summary should be different from the Title. Dont add any tags or hashwords with # and dont tell people were to download or get a script. Dont use the title and header of the text in your response. Be precise as possible without exceeding 18 words in your response. \n\n${content}.`;
+      const prompt = `Summarize the following text. Don´t use the title to summarize the article. Focus on the content. Your summary should be different from the Title. Dont add any tags or hashwords with # and dont tell people were to download or get a script. Dont use the title and header of the text in your response. Be precise as possible without exceeding 18 words in your response. \n\n${content}.`;
       const aiResponse = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: `${prompt}` }],
